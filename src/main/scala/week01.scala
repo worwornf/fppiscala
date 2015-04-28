0b4cb72a1b2ec56129fdd621a783f05a83e7815e
Jeongsang Lee <worwornf@gmail.com>
2015-04-28 09:34:52 +0900

    week01 추가

---
+++ src/main/scala/week01.scala
@@ -0,0 +1,6 @@
+/**
+ * Created by key on 2015-04-28.
+ */
+class week01 {
+
+}
---
+++ src/test/scala/week01Test.scala
@@ -0,0 +1,8 @@
+import org.scalatest.FunSuite
+
+/**
+ * Created by key on 2015-04-28.
+ */
+class week01Test extends FunSuite {
+
+}
